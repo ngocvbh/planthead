49b630c46f34066f7ea415537b3560888ae3b61f
Ngoc Huynh <97254009+ngocvbh@users.noreply.github.com>
2023-11-30 23:53:58 -0800

    included detailed information of plant

--- Planthead-angular/Planthead/src/app/app.module.ts
+++ Planthead-angular/Planthead/src/app/app.module.ts
@@ -1,11 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { PlantIdentifierComponent } from './plant-identifier/plant-identifier.component';
-import { PlantIdentifierService } from './plant-identifier.service';
 
 @NgModule({
   declarations: [
@@ -14,10 +12,9 @@ import { PlantIdentifierService } from './plant-identifier.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
     HttpClientModule
   ],
-  providers: [PlantIdentifierService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Planthead-angular/Planthead/src/app/plant-identifier/plant-identifier.component.ts
+++ Planthead-angular/Planthead/src/app/plant-identifier/plant-identifier.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PlantIdentifierService } from '../plant-identifier.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-plant-identifier',
@@ -8,9 +8,10 @@ import { PlantIdentifierService } from '../plant-identifier.service';
 })
 export class PlantIdentifierComponent {
   selectedImage: File | null = null;
-  suggestions: any[] = [];
+  identifiedPlants: any[] = [];
+  errorMessage: string | null = null;
 
-  constructor(private plantIdentifierService: PlantIdentifierService) { }
+  constructor(private http: HttpClient) { }
 
   onFileSelected(event: any): void {
     this.selectedImage = event.target.files[0];
@@ -18,16 +19,59 @@ export class PlantIdentifierComponent {
 
   identifyPlant(): void {
     if (this.selectedImage) {
-      this.plantIdentifierService.identifyPlant(this.selectedImage)
-        .subscribe(
-          (response: any) => {
-            this.suggestions = response.suggestions;
-            console.log(response, { depth: null });
-          },
-          (error) => {
-            console.error('Error identifying plant:', error);
-          }
-        );
+      const fileReader = new FileReader();
+
+      fileReader.onload = () => {
+        const base64Image = fileReader.result?.toString().split(',')[1];
+
+        if (base64Image) {
+          this.callPlantIdentificationAPI(base64Image);
+        }
+      };
+
+      fileReader.readAsDataURL(this.selectedImage);
     }
   }
+
+  private callPlantIdentificationAPI(base64Image: string): void {
+    const apiUrl = 'https://api.plant.id/v2/identify';
+    const apiKey = 'F5JvjP5w8oTAHujCu056rI76pQBX0uPyTE0fkHfWFiyHZZyw2p';
+
+    
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Api-Key': apiKey
+    });
+
+    const requestBody = {
+      images: [base64Image],
+      modifiers: ['similar_images'],
+      plant_details: ['common_names', 'description', 'edible_parts', 'url']
+    };
+
+    this.http.post(apiUrl, requestBody, { headers }).subscribe(
+      (response: any) => {
+        this.identifiedPlants = response.suggestions.map((suggestion: any) => ({
+          plant_name: suggestion.plant_name,
+          probability: suggestion.probability,
+          common_names: suggestion.plant_details.common_names || [],
+          description: suggestion.plant_details.description,
+          edible_parts: suggestion.plant_details.edible_parts,
+          url: suggestion.plant_details.url || ''
+        }));
+        this.errorMessage = null;
+      },
+      (error) => {
+        this.errorMessage = 'Error identifying the plant. Please try again.';
+        console.error('Error:', error);
+        this.identifiedPlants = [];
+      }
+    );
+  }
+
+  getDescriptionString(description: any): string {
+    return JSON.stringify(description);
+  }
+  
 }
